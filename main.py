0522750782251dcbafea01fff376b8c124b215f2
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2024-06-09 14:11:11 -0700

    add link to github

--- main.py
+++ main.py
@@ -113,6 +113,8 @@ def generate_audio(file: str, openai_api_key: str = None) -> bytes:
 description = """
 <p style="text-align:center">
   <strong>Convert any PDF into a podcast episode! Experience research papers, websites, and more in a whole new way.</strong>
+  <br>
+  <a href="https://github.com/knowsuchagency/pdf-to-podcast">knowsuchagency/pdf-to-podcast</a>
 </p>
 """
 
